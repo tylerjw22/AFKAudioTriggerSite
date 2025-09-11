9a459d7e361002a504ced96b6762d8003e8ade05
Tyler <101249168+tylerjw22@users.noreply.github.com>
2025-09-11 14:06:03 +1000

    Update check-vps.js

--- api/check-vps.js
+++ api/check-vps.js
@@ -1,7 +1,7 @@
 // /pages/api/check-vps.js
 export default async function handler(req, res) {
   try {
-    const vpsRes = await fetch("https://afkaudiotrigger.gnorelist.com/", {
+    const vpsRes = await fetch("https://afkaudiotrigger.ignorelist.com/", {
       method: "GET",
       headers: { "Content-Type": "application/json" }
     });
